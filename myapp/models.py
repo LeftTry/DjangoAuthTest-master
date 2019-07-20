bdeadc6bb1ba0a162b9668b8a3dfbd955e53d347
LeftTry <lerest.go@gmail.com>
2019-07-21 00:04:46 +0300

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from django.db import models
+class Problem(models.Model):
+    text = models.TextField()
+    room = models.PositiveIntegerField()
+    status = models.TextField(default='Не выполнено')
+    author = models.ForeignKey(User, on_delete='models.CASCAD')
---
+++ myapp/views.py
@@ -0,0 +1,85 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+from myapp.models import Problem
+
+
+def index(request):
+    if request.user.is_authenticated:
+        if User.groups == 'Fixer':
+            return render(request, 'fixer.html')
+        else:
+            problems = Problem.objects.all()
+            return render(request, "index.html", {'problems': problems})
+    else:
+        return redirect('/login')
+
+def CreateProblem(request):
+    if request.method == 'GET':
+        return render(request, 'Create.html')
+    if request.method == 'POST':
+        problem = Problem()
+        problem.text = request.POST['Problem']
+        problem.room = request.POST['Number']
+        problem.author = request.user
+        problem.save()
+        return redirect('/')
+
+def login_page(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('login', '')
+        password = request.POST.get('password', '')
+
+        if username == '' or password == '':
+            messages.error(request, 'Заполните все поля!')
+            return redirect('/login')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Неправильный логин или пароль!')
+            return redirect('/login')
+
+
+def register(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'register.html')
+    if request.method == 'POST':
+        username = request.POST.get('login', '')
+        password = request.POST.get('password', '')
+        email = request.POST.get('email', '')
+
+        if username == '' or password == '' or email == '':
+            messages.error(request, 'Заполните все поля')
+            return redirect('/register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Логин занят")
+            return redirect('/register')
+
+        # создаем пользователя
+        user = User.objects.create_user(username, email, password)
+        user.save()
+
+        # "входим" пользователя
+        login(request, user)
+
+        return redirect('/')
+
+def logout_page(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect('/login')
